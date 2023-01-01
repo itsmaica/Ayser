fda2ae6a150a4b82400752a5df8335fa89560e23
Tanner Shaw <traumaaa@gmail.com>
2023-01-01 17:12:29 -0600

    firebase realtime DB setup

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFireStore, collection } from "firebase/firestore"
+import { getDatabase } from "firebase/database"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,13 +14,12 @@ const firebaseConfig = {
   storageBucket: "hackathon-aa.appspot.com",
   messagingSenderId: "15564630310",
   appId: "1:15564630310:web:7a77c60bfca53b33f139e7",
-  measurementId: "G-VJ001K91PV"
+  measurementId: "G-VJ001K91PV",
+  databaseURL: "https://hackathon-aa-default-rtdb.firebaseio.com/"
 };
 
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFireStore(app);
 const auth = getAuth(app);
-
-
+const db = getDatabase(app);
