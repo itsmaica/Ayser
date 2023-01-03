f65b6a1e3959fa36c2335bdfa8816038c9fdd4ad
Tanner Shaw <traumaaa@gmail.com>
2023-01-03 10:09:52 -0600

    pushing front end to project

---
+++ public/components/question-card/index.js
@@ -0,0 +1,65 @@
+export const questionContainer = () => {
+  const roomContainer = document.getElementById("room-container");
+  const container = document.createElement("div");
+
+  container.setAttribute("id", "question-container");
+  // container.setAttribute("class", "room-question-containers");
+
+  const { nameField, questionField, questionButton } = questionInputs();
+
+  container.append(nameField, questionField, questionButton);
+
+  roomContainer.appendChild(container);
+};
+
+const questionInputs = () => {
+  const nameField = document.createElement("div");
+  const nameLabel = document.createElement("label");
+  const nameInput = document.createElement("input");
+
+  const questionField = document.createElement("div");
+  const questionLabel = document.createElement("label");
+  const questionInput = document.createElement("textarea");
+
+  const questionButton = document.createElement("button");
+
+  nameField.setAttribute("class", "input-container");
+  questionField.setAttribute("class", "input-container");
+
+  nameLabel.setAttribute("id", "name-label");
+  nameLabel.innerText = "Name (optional)";
+
+  nameInput.setAttribute("id", "name-input");
+  nameInput.setAttribute("class", "question-inputs");
+
+  questionLabel.setAttribute("id", "question-label");
+
+  questionInput.setAttribute("id", "question-input");
+  questionInput.setAttribute("class", "question-inputs");
+  questionLabel.innerText = "Ask A Question";
+
+  questionButton.setAttribute("id", "question-button");
+  questionButton.setAttribute("class", "submit-buttons");
+  questionButton.innerText = "Submit";
+
+  nameField.append(nameLabel, nameInput);
+  questionField.append(questionLabel, questionInput);
+
+  return { nameField, questionField, questionButton };
+};
+
+// requests
+export const postQuestion = async ({ name, question }) => {
+  const response = await fetch("/questions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, question }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+};
---
+++ public/components/questions/index.js
@@ -0,0 +1,62 @@
+import { highlightIcon, upvoteIcon } from "../icons/reply-icons.js";
+
+export const questionListContainer = () => {
+  const roomContainer = document.getElementById("room-container");
+  const container = document.createElement("div");
+
+  container.setAttribute("id", "question-list-container");
+
+  const questionContent = [
+    "Does flex go on the container? Does flex go on the container?Does flex go on the container?Does flex go on the container?Does flex go on the container?",
+    "Does flex go on the container?",
+    "Does flex go on the container?",
+    "Does flex go on the container?",
+  ];
+
+  // TODO: Fetch questions to render if any
+  for (let i = 0; i < questionContent.length; i++) {
+    const content = questionContent[i];
+
+    // replace i with question id
+    const question = createQuestion(content, i);
+    container.appendChild(question);
+  }
+
+  roomContainer.appendChild(container);
+};
+
+export const createQuestion = (content, id) => {
+  const container = document.createElement("section");
+  container.setAttribute("class", "questions");
+
+  const actionContainer = document.createElement("div");
+  actionContainer.setAttribute("id", "reply-actions-container");
+
+  const question = document.createElement("h2");
+  question.setAttribute("class", "question-content");
+  question.dataset.questionId = id;
+
+  question.innerText = content;
+
+  const replyButton = document.createElement("button");
+  replyButton.setAttribute("id", "reply-button");
+  replyButton.setAttribute("class", "submit-buttons");
+  replyButton.dataset.questionId = id;
+
+  replyButton.innerText = "Reply";
+
+  actionContainer.append(replyButton, highlightIcon(id), upvoteIcon(id));
+  container.append(question, actionContainer);
+
+  return container;
+};
+
+// TODO: fetch questions in question list container
+export const getAllQuestions = async () => {
+  const response = await fetch("/questions");
+
+  if (response.ok) {
+    const data = await response.json();
+    return data;
+  }
+};
