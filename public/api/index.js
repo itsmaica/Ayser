3e7da65321a2e78706aee2b5e701ee0a73596c3f
gootieno <geoff.otieno7@gmail.com>
2023-01-03 23:51:30 -0800

    moved api functions to an api dir

---
+++ public/api/index.js
@@ -0,0 +1,29 @@
+// requests
+// get all replies by question
+export const getQuestionReplies = async (questionId) => {
+  const replyCollection = collection(db, `questions/${questionId}/replies`);
+  const allReplies = await getDocs(replyCollection);
+  return allReplies;
+};
+
+// post question to db
+export const postQuestion = async ({ name, question }) => {
+  const questions = await addDoc(collection(db, "questions"), {
+    name: name,
+    question: question,
+  });
+};
+
+// mark question as highlighted
+export const highLightQuestion = async (questionId, highlighted) => {
+  // TODO: query question db and mark as highlighted
+};
+
+export const upvoteQuestion = async (questionId, voteCount) => {
+  //TODO: increase upvote count
+};
+
+// post reply to db
+export const addReply = async (questionId, reply) => {
+  // TODO: create reply associated to question by questionId
+};
