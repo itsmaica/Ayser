d49d0e8d01aa4b16d13a0fcca1228e497b7f9c81
gootieno <geoff.otieno7@gmail.com>
2023-01-04 10:46:19 -0800

    Added app name to room banner with styles

--- public/components/banner/banner.js
+++ public/components/banner/banner.js
@@ -4,9 +4,11 @@ export const banner = () => {
   const bannerContainer = document.createElement("div");
   bannerContainer.setAttribute("id", "banner-container");
 
+  const bannerHeading = document.createElement("h1");
+  bannerHeading.setAttribute("id", "banner-heading");
 
-  bannerContainer.appendChild(menuContainer());
-
+  bannerHeading.innerText = "AYSR";
+  bannerContainer.append(bannerHeading, menuContainer());
 
   document.body.appendChild(bannerContainer);
 };
